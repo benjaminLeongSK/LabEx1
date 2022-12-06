61acff79fec7e44931a6b45ab7d8c848b8fe2781
benjaminLeongSK <benjaminlsk@live.com>
2022-12-06 21:46:00 +0800

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
-
-
 document.getElementById("rollbtn").addEventListener("click",gameStart);
 
 function gameStart(){
     let number = document.getElementById("number").value;
     const DICEROLL = Math.floor((Math.random() * number) + 1);
     let imageTag = document.getElementById("diceImage");
-    imageTag.src = "dice.img/dice"+DICEROLL+".png"
-}
+    imageTag.src = "dice.img/dice"+DICEROLL+".png";
 
-export function getNumber(){
-    // return Math.floor((Math.random() * number) + 1);
-    return 1;
-}
+    // functionality test
+    if (DICEROLL <= number){
+        console.log("true");
+    }else {
+        alert("error");
+    }
+};
+
